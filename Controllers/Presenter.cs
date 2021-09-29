c5e999e2b7b71fd63b2cbd107302902785c1f7fb
robertdubson <dubson2001@gmail.com>
2021-09-29 15:22:52 +0300

    Added 2 projects for Controller and View for implementing MVP. Connected events from view with methods from Presenter.

---
+++ Controllers/Presenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View;
+namespace Controllers
+{
+    public class Presenter
+    {
+        private IGameView View;
+
+        public Presenter(IGameView view)
+        {
+            View = view;
+
+            View.PlacingTheWall += PlaceTheWall;
+
+            View.PlayerMove += MakeStep;
+        }
+
+        public void MakeStep() {
+
+            throw new NotImplementedException();
+
+        }
+
+        public void PlaceTheWall() {
+
+            throw new NotImplementedException();
+
+        }
+
+
+    }
+}
---
+++ View/IGameView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public interface IGameView
+    {
+        event Action PlacingTheWall;
+
+        event Action PlayerMove;
+    }
+}
