c4fec8d96cab77aaad85df3c0020f9de156109d6
gazinaft <gazinaft@gmail.com>
2021-10-05 19:28:33 +0300

    Command pattern

---
+++ Model/MovePlayerCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Model {
+    public class MovePlayerCommand: ICommand {
+        
+        private readonly int _x;
+        private readonly int _y;
+
+        
+        public MovePlayerCommand(int x, int y, bool direction) {
+            _x = x;
+            _y = y;
+        }
+        
+        public void Execute(GameField field) {
+            field.MovePlayer(_x, _y);
+        }
+    }
+}
---
+++ Model/PlaceWallComand.cs
@@ -0,0 +1,19 @@
+﻿namespace Model {
+    public class PlaceWallCommand: ICommand {
+
+        private readonly int _x;
+        private readonly int _y;
+        private readonly bool _direction;
+
+        
+        public PlaceWallCommand(int x, int y, bool direction) {
+            _x = x;
+            _y = y;
+            _direction = direction;
+        }
+        
+        public void Execute(GameField field) {
+            field.SetBlock(_x, _y, _direction);
+        }
+    }
+}
