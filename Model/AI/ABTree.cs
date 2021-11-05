26ff649e1197920aad27e968c501dfdd23dc7078
gazinaft <gazinaft@gmail.com>
2021-11-05 21:08:16 +0200

    Minimax

--- Model/AI/ABTree.cs
+++ Model/AI/ABTree.cs
@@ -1,29 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Model {
 
     public delegate float SEV(Game game);
-    public class ABTree {
+    public class ABTree: AI{
         
         private Node _root;
-        private Game _game;
         private SEV _sev;
 
-        public ABTree(SEV sev, Game game, int depth) {
+        public ABTree(SEV sev, int depth) {
             _sev = sev;
-            _game = game;
             _root = new Node(new EmptyCommand(), depth);
         }
-
-        public ICommand getBestTurn() {
-            return new EmptyCommand();
-        }
-
-        private void alphaBeta() {
-            int alpha = -1000;
-            int beta = 1000;
-            
-        }
         
+        private void MiniMax(Game game) {
+            var stack = new Stack<Node>();
+            stack.Push(_root);
+            
+            while (stack.Count != 0) {
+                var current = stack.Pop();
+                current.Command.Execute(game);
+                if (current.Level == 0) {
+                    current.BestValue = _sev(game);
+                    current.Undo(game);
+                }
+                else {
+                    var actions = game.GetLegalActions();
+                    // LIFO will recursively go up because it will be the last child node iterating and go up
+                    var backTrack = new Node(actions[0], current.Level - 1, current, !current.Max) { BackTrack = true };
+                    stack.Push(backTrack);
+                    for (int i = 1; i < actions.Count; i++) {
+                        stack.Push(new Node(actions[i], current.Level - 1, current));
+                    }
+                }
+            }
+        }
+
+        public ICommand GetBestMove(Game game) {
+            MiniMax(game);
+            return _root.BestNode.Command;
+        }
     }
 }
--- Model/AI/Node.cs
+++ Model/AI/Node.cs
@@ -1,17 +1,44 @@
-﻿namespace Model {
+﻿using System;
+
+namespace Model {
     public class Node {
 
         public Node Parent;
         public int Level;
         public ICommand Command;
+        public float BestValue;
+        public bool BackTrack = false;
+        public bool Max;
+        public Node BestNode = null;
         
-        public Node(ICommand command, int level, Node parent = null) {
+        public Node(ICommand command, int level, Node parent = null, bool max = true) {
             Command = command;
             Parent = parent;
             Level = level;
+            Max = max;
         }
 
+        private void BestForDepth(Node child) {
+            var childV = child.BestValue;
+            if (BestValue > childV) {
+                if (!Max) {
+                    BestNode = child;
+                    BestValue = childV;
+                }
+            }
+            else {
+                if (Max) {
+                    BestNode = child;
+                    BestValue = childV;
+                }
+            }
+        }
         
-        
+        public void Undo(Game game) {
+            Parent.BestForDepth(this);
+            Command.Undo(game);
+            if(BackTrack) Parent.Undo(game);
+        }
+
     }
 }
