2db1fa9cbd0c2ee6399d76de6939ab0fa0c19822
robertdubson <dubson2001@gmail.com>
2021-12-27 13:44:57 +0200

    IMessage interface

---
+++ ClientServerArchitecture/IMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerArchitecture
+{
+    public interface IMessage
+    {
+        string Title { get; set; }
+
+        int X { get; set; }
+
+        int Y { get; set; }
+
+        string Message { get; set; }
+
+        bool IsHorisontal { get; set; }
+    }
+}
